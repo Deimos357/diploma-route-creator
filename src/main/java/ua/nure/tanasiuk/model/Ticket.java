fb307a8228610ae07f5760213b720ec56955eec8
tanasiuk <dmitriy.tanasiuk@nixsolutions.com>
2018-06-04 17:07:02 +0300

    edit works

--- src/main/java/ua/nure/tanasiuk/model/Ticket.java
+++ src/main/java/ua/nure/tanasiuk/model/Ticket.java
@@ -1,11 +1,13 @@
 package ua.nure.tanasiuk.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ua.nure.tanasiuk.resource.deserializer.UnixDateDeserializer;
 import ua.nure.tanasiuk.resource.serializer.UnixDateSerializer;
 
 import java.util.Date;
@@ -21,6 +23,7 @@ public class Ticket {
     private double cost;
     private int duration;
     @JsonSerialize(using = UnixDateSerializer.class)
+    @JsonDeserialize(using = UnixDateDeserializer.class)
     private Date departureTime;
     private int transportTypeId;
     private int fromId;
